d1382040e0aa5c45056629236c70684122b16371
xaatw <>
2015-07-21 14:34:35 +0900

    mavenの基本スタイルのみ

---
+++ sql2bean/src/main/java/com/github/xaatw0/sql2bean/App.java
@@ -0,0 +1,13 @@
+package com.github.xaatw0.sql2bean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
