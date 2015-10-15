37ed01318a19bf0e7674bdecf1424d095fb57742
s-sakurama <s-sakurama@PC-K115>
2015-10-15 13:56:26 +0900

    loadボタンを追加

--- sql2bean/src/main/java/com/github/xaatw0/sql2bean/fx/FXController.java
+++ sql2bean/src/main/java/com/github/xaatw0/sql2bean/fx/FXController.java
@@ -1,7 +1,6 @@
 package com.github.xaatw0.sql2bean.fx;
 
 import java.net.URL;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.beans.property.SimpleStringProperty;
@@ -13,7 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.cell.MapValueFactory;
+
+import com.github.xaatw0.sql2bean.sql.ColumnInfo;
 
 public class FXController implements Initializable{
 
@@ -21,7 +21,7 @@ public class FXController implements Initializable{
 	private LogicInterface logic;
 
 	@FXML private TextArea txtSql;
-	@FXML private TableView tableResult;
+	@FXML private TableView<ColumnInfo> tableResult;
 	@FXML private Button btnExecute;
 	@FXML private Button btnSave;
 
@@ -44,14 +44,19 @@ public class FXController implements Initializable{
 	public void save(ActionEvent event){
 	}
 
+	@FXML
+	public void load(ActionEvent event){
+	}
+
     /**
      * 項目を追加する
      * @param table
      * @param columnName
      */
-    private void addColumn(TableView table, String columnName) {
-        TableColumn<Map, String> column = new TableColumn<>(columnName);
-        column.setCellValueFactory(new MapValueFactory(columnName));
+    private void addColumn(TableView<ColumnInfo> table, String columnName) {
+
+        TableColumn<ColumnInfo, String> column = new TableColumn<>(columnName);
+        //column.setCellValueFactory(new MapValueFactory<ColumnInfo>(columnName));
         column.setMinWidth(130);
         // column.setCellFactory(cellFactoryForMap);
         table.getColumns().add(column);
